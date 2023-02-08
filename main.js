e2b4e4ff4ddf7629e9382c938fb3f85fabe39295
kimanikevin254 <kimanikevin254@gmail.com>
2023-02-08 17:17:47 +0300

    created ui

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 const { app, BrowserWindow } = require('electron')
 const path  = require('path')
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+const isMac = process.platform === 'darwin';
+
+const isDev = process.env.NODE_ENV !== 'production'
 
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
         height: 600,
-        width: 500
+        width: isDev ? 1000: 500
     })
+
+    // open dev tools if in dev env
+    if(isDev){
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
 
 app.whenReady().then(() => {
     createMainWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+          createMainWindow()
+        }
+      })
 })
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+      app.quit()
+    }
+  })
---
+++ renderer/js/script.js
@@ -0,0 +1,5 @@
+// Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
+
+const form = document.querySelector('#img-form');
+
+f
